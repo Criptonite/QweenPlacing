a708a56661f5f607420f44cb38c669f7af27b876
flyerRUS <shtorm5945@yandex.ru>
2017-06-26 15:48:59 +0200

    Fixed desk
    Fixed desk

--- src/objects/Desk.java
+++ src/objects/Desk.java
@@ -16,13 +16,10 @@ import java.util.ArrayList;
 public class Desk {
 
     //Image owner needs to be implemented in separate class
-    private BufferedImage qweenImage;
     private int size;
     private double cellSize;
-    Graphics2D graphics2D;
     private Cell[][] cellMatrix;
     private Backtracking backtracking;
-    private GraphPanel graphPanel; //NEED
     private ArrayList<Cell[][]> steps;
     private ArrayList<Cell[][]> combinations;
 
@@ -46,15 +43,11 @@ public class Desk {
         }
     }
 
-    public Desk(int size, GraphPanel graphPanel, Graphics2D graphics2D) {
+    public Desk(int size, double graphPanelSize) {
         this.size = size;
-        this.graphPanel = graphPanel;
-        this.graphics2D = graphics2D;
-        this.cellSize = graphPanel.getWidth() / size;
+        this.cellSize = graphPanelSize / size;           //!!!!!!!!!!!!!!!!
         cellMatrix = new Cell[size][size];
         fillMatrix();
-        QueenImage img = new QueenImage();
-        qweenImage = img.resize((int) cellSize, (int) cellSize);
     }
 
     /**
@@ -66,50 +59,10 @@ public class Desk {
         return cellMatrix;
     }
 
-    /**
-     * Method painting desk
-     */
-    public void paintDesk() {
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                drawElement(i, j);
-            }
-        }
-    }
-
-
-    /**
-     * Method used for drawing cells in one string
-     *
-     * @param i index of raw
-     * @param j index of coll
-     */
-    private void drawElement(int i, int j) {
-        Cell cell = cellMatrix[i][j];
-
-        graphics2D.setColor(cell.getStandardColor());
-        graphics2D.fill(cell);
-        graphics2D.draw(cell);
-
-        graphics2D.setColor(cell.getColor());
-        graphics2D.fill(cell);
-        graphics2D.draw(cell);
-
-        if (qweenImage != null)
-            if (cell.isQueen())
-                graphics2D.drawImage(qweenImage, (int) cell.getX(), (int) cell.getY(), graphPanel);
-    }
-
-    public void setGraphics2D(Graphics2D graphics2D) {
-        this.graphics2D = graphics2D;
-    }
-
-
     public void searchSolutions() {
         backtracking = new Backtracking(this);
         backtracking.run();
     }
-
 
     public ArrayList<Cell[][]> getCombinations() {
         return combinations;
@@ -131,4 +84,7 @@ public class Desk {
         this.cellMatrix = cellMatrix;
     }
 
+    public double getCellSize() {
+        return cellSize;
+    }
 }
